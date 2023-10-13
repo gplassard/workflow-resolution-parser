345c367f2c3dda3c4db98a55eb0a6cb06010a012
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-10-14 01:22:46 +0200

    feat(parsing): parson json literals

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,10 @@ fn main() {
 fn parse(input: &str) -> IResult<&str, ResolvedValue> {
     let (remaining, parsed) = many0(anychar)(input)?;
     let string: String = parsed.into_iter().collect();
+    let parsed: Value = serde_json::from_str(&*string).unwrap(); // FIXME
     Ok((
         remaining,
-        ResolvedValue::JsonValue { value: json!(string) }
+        ResolvedValue::JsonValue { value: parsed }
     ))
 }
 
@@ -35,8 +36,36 @@ mod tests {
 
     #[test]
     fn test_parse_whole_string() {
-        let result = parse("Hello world");
+        let result = parse("Hello world"); // FIXME
         let expected = Ok(("", JsonValue {value: json!("Hello world")}));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_json_string() {
+        let result = parse("\"Hello world\"");
+        let expected = Ok(("", JsonValue {value: json!("Hello world")}));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_json_true() {
+        let result = parse("true");
+        let expected = Ok(("", JsonValue {value: json!(true)}));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_json_false() {
+        let result = parse("false");
+        let expected = Ok(("", JsonValue {value: json!(false)}));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_json_number() {
+        let result = parse("42");
+        let expected = Ok(("", JsonValue {value: json!(42)}));
         assert_eq!(result, expected);
     }
 }
