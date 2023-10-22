0a96b6e80075815b130002443268bfd6d704e21d
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-10-23 00:59:25 +0200

    feat(parsing): setup parsing

---
+++ src/parse/mod.rs
@@ -0,0 +1,15 @@
+use nom::branch::alt;
+use nom::IResult;
+
+use crate::expression::Expression;
+use crate::parse::parse_boolean::parse_boolean;
+use crate::parse::parse_string::parse_string_as_expression;
+use crate::parse::parse_template::parse_template;
+
+mod parse_boolean;
+mod parse_string;
+mod parse_template;
+
+pub fn parse_expression(input: &str) -> IResult<&str, Expression> {
+    alt((parse_template, parse_boolean, parse_string_as_expression))(input)
+}
---
+++ src/parse/parse_boolean.rs
@@ -0,0 +1,57 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag_no_case;
+use nom::combinator::value;
+use nom::IResult;
+use serde_json::json;
+
+use crate::expression::Expression;
+
+pub fn parse_boolean(input: &str) -> IResult<&str, Expression> {
+    alt((
+        value(
+            Expression::JsonValue { value: json!(true) },
+            tag_no_case("true"),
+        ),
+        value(
+            Expression::JsonValue { value: json!(false) },
+            tag_no_case("false"),
+        )
+    ))(input)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::expression::Expression::JsonValue;
+    use crate::parse::parse_boolean::parse_boolean;
+
+    #[test]
+    fn test_parse_true() {
+        let result = parse_boolean("true");
+        let expected = Ok(("", JsonValue { value: json!(true) }));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_false() {
+        let result = parse_boolean("false");
+        let expected = Ok(("", JsonValue { value: json!(false) }));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_case() {
+        let result = parse_boolean("TrUe");
+        let expected = Ok(("", JsonValue { value: json!(true) }));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_remain() {
+        let result = parse_boolean("TrUeAndOtherStuff");
+        let expected = Ok(("AndOtherStuff", JsonValue { value: json!(true) }));
+        assert_eq!(result, expected);
+    }
+}
