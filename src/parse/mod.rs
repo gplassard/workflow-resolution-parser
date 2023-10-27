a1a28db0b7732de063da5bcfb4c21f0895d38193
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-10-27 23:57:37 +0200

    chore(path-element): parse path elements

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -9,6 +9,8 @@ use crate::parse::parse_template::parse_template;
 mod parse_boolean;
 mod parse_string;
 mod parse_template;
+mod parse_template_expression;
+mod parse_path_element;
 
 pub fn parse_expression(input: &str) -> IResult<&str, Expression> {
     alt((parse_template, parse_boolean, parse_string_as_expression))(input)
