21c62686d61e5ccfd74d3b3e1699a8e49f975887
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-11-14 20:09:06 +0100

    wip

--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -26,14 +26,41 @@ pub enum PathElement {
     IndexPath { index: i32 },
 }
 
-pub fn evaluate(exp: Expression) -> Expression {
+pub fn evaluate(exp: Expression, context: Value) -> Expression {
     match exp {
         Expression::JsonValue { value } => Expression::JsonValue { value },
         Expression::TemplateExpression { expression } => Expression::JsonValue {
-            value: json!(format!("{{ {:?} }}", expression)),
+            value: evaluate_template_expression(expression, context),
         },
         Expression::StringConcatenation { parts } => Expression::JsonValue {
             value: json!(format!("{{ {:?} }}", parts)),
         },
+    }
+}
+
+pub fn evaluate_template_expression(exp: TemplateExpression, context: Value) -> Value {
+    match exp {
+        TemplateExpression::FieldAccessor { path } => {
+            evaluate_field_accessor(path, context)
+        }
+    }
+}
+
+pub fn evaluate_field_accessor(path: Vec<PathElement>, context: Value) -> Value {
+    let mut current_value = context;
+    for path_element in path {
+        current_value = evaluate_path_element(path_element, current_value);
+    }
+    current_value
+}
+
+pub fn evaluate_path_element(path_element: PathElement, context: Value) -> Value {
+    match path_element {
+        PathElement::AttributePath { name } => {
+            context.get(name).unwrap().clone()
+        }
+        PathElement::IndexPath { index } => {
+            context.get(index as usize).unwrap().clone()
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde_json::json;
 use crate::expression::evaluate;
 use crate::parse::parse_expression;
 
@@ -5,7 +6,16 @@ mod expression;
 mod parse;
 
 fn main() {
-    let exp = parse_expression("{{trUe}}");
+    let exp = parse_expression("{{foo.bar[1].baz}}");
     println!("{:?}", exp);
-    println!("{:?}", evaluate(exp.unwrap().1));
+    println!("{:?}", evaluate(exp.unwrap().1, json!({
+        "foo": {
+            "bar": [
+                {},
+                {
+                    "baz": "foobar"
+                },
+            ]
+        }
+    })));
 }
